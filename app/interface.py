e16e625b1b4d6a1cb21a74317e06ebb8fc7a4926
lucas_lsg <lucas.goncalves@fit-tecnologia.org.br>
2023-06-16 00:13:13 -0400

    Add pagination feature

--- app/interface.py
+++ app/interface.py
@@ -17,14 +17,45 @@ class OpenInterface(QWidget):
         # Command to not allow maximize window
         self.setWindowFlags(self.windowFlags() & ~Qt.WindowMaximizeButtonHint)
         self.path = ""
-        
         self.initUI()
 
+    def show_image(self):
+        self.exec_utils = AccessUtils(self.path)
+        img_list = self.exec_utils.mainImages(self.path)
+        self.img_label = QLabel(self)
+        self.img_label.setGeometry(150, 350, 300, 450)
+
+        image_path = img_list[self.current_index]
+        pixmap = QPixmap(image_path)
+        self.img_label.setPixmap(pixmap)
+        self.img_label.setScaledContents(True)
+        self.img_label.show()
+
+        img_name = image_path.split("data")[-1]
+        self.name_label = QLabel(self)
+        self.name_label.setGeometry(150, 800, 100, 50)
+        self.name_label.setText(img_name)
+        self.name_label.show()
+
+    def show_previous_image(self):
+        self.exec_utils = AccessUtils(self.path)
+        img_list = self.exec_utils.mainImages(self.path)
+        self.current_index -= 1
+        if self.current_index < 0:
+            self.current_index = len(img_list) - 1
+        self.show_image()
+
+    def show_next_image(self):
+        self.exec_utils = AccessUtils(self.path)
+        img_list = self.exec_utils.mainImages(self.path)
+        self.current_index += 1
+        if self.current_index >= len(img_list):
+            self.current_index = 0
+        self.show_image()
+
     def openWindow(self):
-        # Seta as coordenadas da janela e o titulo
         self.setGeometry(0, 0, 1200, 900)
         self.setWindowTitle('BugHunters System Interface')
-        # Centraliza a janela na tela principal
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
@@ -32,7 +63,7 @@ class OpenInterface(QWidget):
 
     def setTitle(self):
         title_label = QLabel(self)
-        title_label.setText("Interface do Sistema")
+        title_label.setText("System Interface")
         title_label.setGeometry(350, 30, 300, 80)
         font = QFont()
         font.setPointSize(16)
@@ -41,7 +72,7 @@ class OpenInterface(QWidget):
 
     def resultsLabel(self):
         title_label = QLabel(self)
-        title_label.setText("Resultado dos Testes")
+        title_label.setText("Test Results")
         title_label.setGeometry(700, 300, 300, 50)
         font = QFont()
         font.setPointSize(10)
@@ -50,7 +81,7 @@ class OpenInterface(QWidget):
 
     def imagesLabel(self):
         title_label = QLabel(self)
-        title_label.setText("Imagens processadas")
+        title_label.setText("Processed Images")
         title_label.setGeometry(150, 300, 300, 50)
         font = QFont()
         font.setPointSize(10)
@@ -63,24 +94,14 @@ class OpenInterface(QWidget):
         self.label.setText("Input: ")
         self.label.move(100, 200)
         self.label.resize(300, 30)
-    
-    def display_image(self):
-        self.label = QLabel(self)
-        self.label.setGeometry(150, 350, 300, 450)
-        self.exec_utils = AccessUtils(self.path)
-        img_list = self.exec_utils.mainImages(self.path)
-        for img in img_list:
-            pixmap = QPixmap(img)
-            self.label.setPixmap(pixmap)
-            self.label.setScaledContents(True)
-            self.label.show()
 
     def create_json_display(self,json_data):
         if json_data != {}:
             self.progress_bar.setFormat('Completed')
             self.text_edit.setPlainText(json_data)
             self.progress_bar.setVisible(False)
-            self.display_image()
+            self.back_button.setEnabled(True)
+            self.next_button.setEnabled(True)
 
     def start_progress(self):
         self.progress_bar = QProgressBar(self)
@@ -111,9 +132,8 @@ class OpenInterface(QWidget):
         self.inputLabel()
         # Input directory
         self.input_text = QLineEdit(self)
-        self.input_text.setToolTip('Insert your data path')
-        self.input_text.move(200, 200)  # Seta posição do campo
-        self.input_text.resize(300, 30)  # Seta tamanho do campo
+        self.input_text.move(200, 200)
+        self.input_text.resize(300, 30)
         self.input_text.textChanged.connect(self.enable_start_buttons)
         self.path = self.input_text.text()
         # Button to start YoLo process
@@ -141,6 +161,20 @@ class OpenInterface(QWidget):
          # Set background color
         self.setStyleSheet("color: rgb(255,255,255); background-color: rgb(50,50,50);")
 
+        self.current_index = 0 
+        self.name_label = QLabel()
+        self.img_label = QLabel()
+        self.back_button = QPushButton('Back',self)
+        self.back_button.move(150, 850)
+        self.back_button.setEnabled(False)
+        self.back_button.clicked.connect(self.show_previous_image)
+
+        self.next_button = QPushButton('Next',self)
+        self.next_button.move(350, 850)
+        self.next_button.setEnabled(False)
+        self.next_button.clicked.connect(self.show_next_image)
+
+
     def enable_start_buttons(self):
         self.path = self.input_text.text()
         self.btn_ocr.setEnabled(False)
@@ -154,8 +188,6 @@ class OpenInterface(QWidget):
             self.btn_yolo.setEnabled(False)
             print("Invalid path")
         else:
-            # self.btn_ocr.setToolTip('Click here')
-            # self.btn_yolo.setToolTip('Click here')
             self.btn_ocr.setEnabled(True)
             self.btn_yolo.setEnabled(True)
 
@@ -170,7 +202,8 @@ class OpenInterface(QWidget):
             self.exec_utils = AccessUtils(self.path)
             self.exec_utils.mainYolo(self.path)
             self.progress_bar.setVisible(False)
-            self.display_image()
+            self.back_button.setEnabled(True)
+            self.next_button.setEnabled(True)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
